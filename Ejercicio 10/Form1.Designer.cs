88e97d810a0fda6ac556a63d8bc06b2b9edf82bf
RaymondHub-Lgtm <raymondismael@icloud.com>
2025-06-14 01:58:54 +0200

    Ejercicio 10

---
+++ Ejercicio 10/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Ejercicio_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            botonmostrarmensaje = new Button();
+            SuspendLayout();
+            // 
+            // botonmostrarmensaje
+            // 
+            botonmostrarmensaje.Location = new Point(50, 74);
+            botonmostrarmensaje.Name = "botonmostrarmensaje";
+            botonmostrarmensaje.Size = new Size(75, 23);
+            botonmostrarmensaje.TabIndex = 0;
+            botonmostrarmensaje.Text = "Mostrar Mensaje";
+            botonmostrarmensaje.UseVisualStyleBackColor = true;
+            botonmostrarmensaje.Click += botonmostrarmensaje_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(botonmostrarmensaje);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button botonmostrarmensaje;
+    }
+}
---
+++ Ejercicio 10/Form1.cs
@@ -0,0 +1,15 @@
+namespace Ejercicio_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void botonmostrarmensaje_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola, Soy Raymond Ismael  y Mi Matricula LR-2024-01392");
+        }
+    }
+}
